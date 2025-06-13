efc7be752c52f23a7c15533bc916ebde08c48ffc
unknown <adefemioluwatobi13@gmail.com>
2025-06-13 12:27:54 +0100

    new

--- db.js
+++ db.js
@@ -16,3 +16,23 @@ db.connect((err) => {
 });
 
 module.exports = db;
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'tramway.proxy.rlwy.net',
+  user: 'root',
+  password: 'PGEaxsWNIsOYJJzdSCoAraQyYroBTKRe',
+  database: 'railway',
+  port: 58336,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    throw err;
+  }
+  console.log('Connected to Railway MySQL database');
+});
+
+module.exports = connection;
